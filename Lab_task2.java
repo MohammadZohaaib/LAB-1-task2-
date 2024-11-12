6d555371bb111cac8131484a971e0617fea7fa51
Muhammad Zohaib Raza <133108370+MohammadZohaaib@users.noreply.github.com>
2024-11-12 22:31:10 +0500

    task 2

---
+++ Lab_task2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_task2;
+
+/**
+ *
+ * @author USER
+ */
+public class Lab_task2 {
+
+    public static void main(String[] args)
+{
+ double primitive=7.88;
+ Double wapp=primitive;
+ System.out.println("Primitive Double Value is"+primitive);
+ System.out.println("Primitive Double Value is"+wapp);
+}
+}
